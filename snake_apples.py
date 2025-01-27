153931d9918aef39d527cf24da99bd41020c4502
Suzanne Azier <204134@stmichaelcollege.nl>
2025-01-27 09:49:06 +0100

    Design appels

--- snake_apples.py
+++ snake_apples.py
@@ -1,29 +1,24 @@
 import pygame
-from snake_settings import Settings
-from snake_game_functions import Game_functions
 from random import randint
-
-# Initialiseer pygame
-pygame.init()
-
-# initialiseer de modules
-settings = Settings()
-gf = Game_functions()
 
 class Apple():
     def __init__(self):
         self.apple_list = []
         self.x = 0
         self.y = 0
-    
-    def draw(self, game_over):
-        # Teken de slang
+        self.image = pygame.image.load("appel.png").convert_alpha()
+        width = 30
+        height = 30
+        self.imageSmall = pygame.transform.scale(self.image, (width, height))
+
+    def draw(self, screen, game_over):
         if not game_over:
-            if not self.apple_list:
-                self.x = randint(0,800-settings.apple_width)
-                self.y = randint(0,600-settings.apple_height)
-            pygame.draw.rect(settings.screen, settings.red, (self.x,self.y, settings.apple_width, settings.apple_height))
-            self.apple_list.append('apple')
+            if not self.apple_list:  # Als er geen appel is, genereer een nieuwe
+                width = 30
+                height = 30
+                self.x = randint(0, 800 - width)
+                self.y = randint(0, 600 - height)
+                self.apple_list.append('apple')  # Voeg een appel toe aan de lijst
 
-
-    
+            # Teken de appel op het scherm
+            screen.blit(self.imageSmall, (self.x, self.y))
