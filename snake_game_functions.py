07a7198573e70a152241db50e8303fb390ded63a
Amber Slob <204987@stmichaelcollege.nl>
2025-01-29 14:54:51 +0100

    fixed issue 6

--- snake_game_functions.py
+++ snake_game_functions.py
@@ -16,6 +16,7 @@ class Game_functions():
         self.right = False
         self.up = False
         self.down = False
+        self.start_right = False
         
     def check_keydown_events(self, game_over):
         #print("checking events..")
@@ -25,18 +26,19 @@ class Game_functions():
                 
             # Beweeg de slang alleen als het spel niet over is
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP and not self.down:
+                if event.key == pygame.K_UP and not (self.down or self.start_right):
                     self.up = True
                     self.left = self.right = False
-                elif event.key == pygame.K_DOWN and not self.up:
+                elif event.key == pygame.K_DOWN and not (self.up or self.start_right):
                     self.down = True
                     self.left = self.right = False
-                elif event.key == pygame.K_LEFT and not self.right:
+                elif event.key == pygame.K_LEFT and not (self.right or self.start_right):
                     self.left = True
                     self.up = self.down = False
                 elif event.key == pygame.K_RIGHT and not self.left:
                     self.right = True
                     self.up = self.down = False
+                    self.start_right = False
                     
         return self.up, self.down, self.left, self.right
     
@@ -47,4 +49,5 @@ class Game_functions():
         self.y = 250
         self.left = self.right = self.up = self.down = False
         self.moves_list.clear()
+        self.start_right = True
         
